14641fe6a3cc2872258b897ab9f90d0cb3c309b4
muhafidz-ahmad <muhafidz.ahmad@gmail.com>
2023-06-03 21:49:21 +0700

    add app file

---
+++ app.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+import streamlit as st
+import xlwings as xw
+
+import generate_qr_code
+import scan_qr_code
+
+# read and prepare the data
+wb = xw.Book('data.xlsx')
+worksheet = wb.sheets('Sheet1');
+
+peserta = worksheet['A2'].expand('down').value
+
+total_peserta = len(peserta)
+
+scan, generate, stop = st.tabs(['Scan QR Code',
+                                'Generate QR Code',
+                                'Stop the Program'])
+
+with stop:
+    if st.button("Stop the Program"):
+        st.write("Program stopped")
+        st.stop()
+
+with scan:
+    "# Scan QR Code"
+    if st.checkbox('Open Camera'):
+        st.write("Press \"q\" to stop the camera.")
+
+        # Panggil fungsi scan_qr_code untuk memulai pemindaian
+        scan_qr_code.scan_qr_code(wb, worksheet, peserta)
+
+with generate:
+    "# Generate New QR Code"
+    # Data yang ingin diubah menjadi QR Code
+    nama = st.text_input("Write you name", "")
+
+    if st.button("Generate QR Code"):
+        # Contoh penggunaan
+        filename = nama + ".png"  # Nama file untuk menyimpan QR Code
+
+        img = generate_qr_code.generate_qr_code(nama, filename)
+        img.save("qrcode/" + filename)
+
+        # Tulis data ke excel
+        total_peserta += 1
+        worksheet['A' + str(total_peserta + 1)].value = nama
+
+        st.write("QR Code {filename} telah dibuat: ")
+        st.image("qrcode/" + filename, caption=filename)
---
+++ generate_qr_code.py
@@ -0,0 +1,17 @@
+import qrcode
+
+import streamlit as st
+
+def generate_qr_code(data, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    # img.save(filename)
+    return img
